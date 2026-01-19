7da4c5071956f546fee0120c4a50f015156f40e1
laplace-anon <150580554+laplace-anon@users.noreply.github.com>
2026-01-20 00:57:06 +0800

    style: update AboutDialog component for improved text presentation and user experience

--- src/components/comet/AboutDialog.tsx
+++ src/components/comet/AboutDialog.tsx
@@ -22,13 +22,15 @@ export function AboutDialog() {
           <DialogDescription>应用程序信息</DialogDescription>
         </DialogHeader>
         <DialogPanel>
-          <div className='mt-10 space-y-4'>
+          <div className='mt-10 select-none space-y-4'>
             <div className='flex flex-col items-center gap-2'>
               <img src={appIcon} alt='LAPLACE Comet' className='size-16' />
               <div className='space-y-1 text-center'>
                 <h3 className='font-bold font-logo text-3xl'>LAPLACE Comet</h3>
-                <p className='text-muted-foreground'>Privacy-first Bilibili Private Message Manager</p>
-                <p className='text-muted-foreground text-sm'>Version {version}</p>
+                <p className='text-muted-foreground'>Privacy-first Bilibili PM Manager</p>
+                <p className='text-muted-foreground text-sm'>
+                  Version <span className='select-text'>{version}</span>
+                </p>
               </div>
             </div>
 
